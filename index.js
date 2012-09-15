a304dc258530f7f34f4ce2d7b3d3e87bcce64565
Enrico Marino <enrico.marino@email.com>
2012-09-15 16:28:51 +0200

    added vector2.set

--- index.js
+++ index.js
@@ -32,6 +32,22 @@
     return new Float32Array([0.0, 0.0]);
   };
 
-  
+  /**
+   * set
+   * Set a 2d vector.
+   * 
+   * @param {Float32Array} self destination vector
+   * @param {Float32Array} v sorce vector
+   * @return {Float32Array} a 2d vector
+   * @api public
+   */
+
+  vec2.set = function (self, v) {
+    self[0] = v[0];
+    self[1] = v[1];
+
+    return self;
+  };
+
 
  }(this));
