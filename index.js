188f3f4a29820e2eb28eeeaf49fb976bb9d5e10f
Enrico Marino <enrico.marino@email.com>
2012-09-15 16:00:52 +0200

    added vector2.create

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-module.exports = require('./lib/vector2.js');
+
+/* !
+ * vector2
+ * JavaScript math library for 2d vector
+ * Copyright (c) 2012 Enrico Marino (http://onirame.no.de)
+ * MIT License
+ */
+
+ !(function (exports) {
+
+  /**
+   * Library namespace.
+   */
+
+  var vector2 = exports.vector2 = {};
+
+  /**
+   * Library version.
+   */
+
+  vector2.version = '0.0.0';
+
+  /**
+   * create
+   * Create a 2d vector.
+   * 
+   * @return {Float32Array} a 2d vector
+   * @api public
+   */
+
+  vector2.create = function () {
+    return new Float32Array([0.0, 0.0]);
+  };
+
+  
+
+ }(this));
